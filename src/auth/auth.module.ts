6987bbc63e140bd670fdd12b34fabe7556b28ddf
ghospadi <Lolr2223@gmail.com>
2022-12-05 17:50:40 +0200

    JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,29 @@
-import { AuthModel } from './auth.model/auth.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModel } from './auth.model/user.model';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from 'src/configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
   imports: [
     TypegooseModule.forFeature([
       {
-        typegooseClass: AuthModel,
+        typegooseClass: UserModel,
         schemaOptions: {
-          collection: 'Auth',
+          collection: 'User',
         },
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig
+    })
   ],
+  providers: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/dto/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_EXISTS = 'Пользователь уже зарегистрирован'
+export const USER_NOT_FOUND = 'Пользователь с таким email не зарегистрирован'
+export const WRONG_PASSWORD = 'Введен не правильный пароль'
